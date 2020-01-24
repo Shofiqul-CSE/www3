fae6d60a52f470d177b26e4fb1386793d27bb7e1
Shofiqul-CSE <52854277+Shofiqul-CSE@users.noreply.github.com>
2020-01-24 21:55:44 +0600

    Add files via upload

---
+++ js/my.js
@@ -0,0 +1,23 @@
+/*-------------------Nav Bar Css Start------------------*/
+
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	$('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
+  });
+
+})(jQuery);
+
+/*-------------------Nav Bar Js End------------------*/
